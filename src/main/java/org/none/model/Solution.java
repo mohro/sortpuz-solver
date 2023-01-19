25207527062eb65e36f46a16e1e990df8f2df251
moh ro <codemoro@outlook.com>
2023-01-19 16:33:25 +0530

    Defined equals and hashcode for Solution

--- src/main/java/org/none/model/Solution.java
+++ src/main/java/org/none/model/Solution.java
@@ -1,6 +1,7 @@
 package org.none.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Solution {
     private final List<Step> steps;
@@ -11,5 +12,18 @@ public class Solution {
 
     public List<Step> steps() {
         return steps;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Solution solution = (Solution) o;
+        return Objects.equals(steps, solution.steps);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(steps);
     }
 }
